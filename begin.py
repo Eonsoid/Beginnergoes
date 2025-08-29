44d35918fd33b079b555362c90c35c77b9517b7d
Rohan Das <8rohandas@gmail.com>
2025-08-29 06:53:02 +0530

    Testing purpose....

---
+++ begin.py
@@ -0,0 +1,2 @@
+print("Hello everyone, I Rohan Das has succesfully installed Git inside my Pop! os and yeah till now i don't know how to use python very well. But I'll be learning that pretty soon. First my focus is on Git then CN. Basically I'll become a DevOps Cloud engineer. Tata ")
+
